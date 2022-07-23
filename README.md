72eed6f33a2f825c23798a429be94c470114eeca
Amine Khouya <amine.khouya1999@gmail.com>
2022-07-23 14:17:04 +0100

    build function command_string that split and handle  && '

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # minishell
 
 # first step is build lexer for the parsing part
-# need build a split function for minishell that split with space and handle ' and " error 
+# need build a split function for minishell that split with space and handle ' and " error
--- minishell.c
+++ minishell.c
@@ -6,20 +6,102 @@
 /*   By: akhouya <akhouya@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 15:01:14 by akhouya           #+#    #+#             */
-/*   Updated: 2022/07/22 17:57:57 by akhouya          ###   ########.fr       */
+/*   Updated: 2022/07/23 14:14:09 by akhouya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
 char **command_string(char *command)
 {
 	char	**s;
 	int i;
+	int j;
+	char c;
+	int count;
+
 	
-	s = ft_split(command, ' ');
+	// s = ft_split(command, ' ');
+	// while
+	s =NULL;
+	i = 0;
+	j = 0;
+	count = 0;
+	while (command[i])
+	{
+		if (command[i] == '\''  || command[i] == '"')
+		{
+			j = i + 1;
+			c = command[i];
+			while (command[i]  != c && command[i])
+				i++;
+			// printf("%c\n", command[i]);
+			if (!command[i])
+				exit(1);
+			if(command[i + 1] == ' ' || !command[i + 1])
+				count++;
+		}
+		else if (command[i] == ' ' || i == 0)
+		{
+			j = i + 1;
+			while (command[i]  != ' ' && command[i] && command[i] != '\'' && command[i] != '"')
+				i++;
+			// {
+			// 	if (command[i] == '\'' || command[i] == '"')
+			// 	{
+			// 		c = command[i];
+			// 		while (command[++i]  != c && command[i]);
+			// 		if (!command[i])
+			// 			exit(1);
+			// 	}
+			// }
+			if (command[i] == ' ' || !command[i])
+				count++;
+		}
+		i++;
+	}
+	s = malloc(sizeof(char*) * (count + 1));
+	s[count] = 0;
+	count = 0;
 	i = -1;
+	j = 0;
+	while (command[i])
+	{
+		if (command[i] == '\''  || command[i] == '"')
+		{
+			j = i + 1;
+			c = command[i];
+			while (command[i]  != c && command[i])
+				i++;
+			// if (!command[i])
+			// 	exit(1);
+			if(command[i + 1] == ' ' || !command[i + 1])
+				s[count++] = ft_substr(&command[j],0, i - j);
+		}
+		else if (command[i] == ' ' || i == 0)
+		{
+			j = i + 1;
+			if (i == 0)
+				j--;
+			// while (command[++i]  != ' ' && command[i])
+			// {
+			// 	if (command[i] == '\'' || command[i] == '"')
+			// 	{
+			// 		c = command[i];
+			// 		while (command[++i]  != c && command[i]);
+			// 		if (!command[i])
+			// 			exit(1);
+			// 	}
+			// }
+			while (command[i]  != ' ' && command[i] && command[i] != '\'' && command[i] != '"')
+				i++;
+			if (command[i] == ' ' || !command[i])
+				s[count++] = ft_substr(&command[j], 0, i - j);
+			// printf("%d\n", i);
+		}
+		i++;
+	}
+	printf("%d\n", count);
 	return s;
 }
 
